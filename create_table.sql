735daded0c235e87c3e2b331844dfc23511d9bd9
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2019-03-28 17:30:34 +0800

    Initial commit

---
+++ create_table.sql
@@ -0,0 +1,34 @@
+CREATE TABLE main (
+    the_villages_and_towns_urban_district text,
+    transaction_sign text,
+    land_sector_position_building_sector_house_number_plate text,
+    land_shifting_total_area_square_meter text,
+    the_use_zoning_or_compiles_and_checks text,
+    the_non_metropolis_land_use_district text,
+    non_metropolis_land_use text,
+    transaction_year_month_and_day text,
+    transaction_pen_number text,
+    shifting_level text,
+    total_floor_number text,
+    building_state text,
+    main_use text,
+    main_building_materials text,
+    construction_to_complete_the_years text,
+    building_shifting_total_area text,
+    building_present_situation_pattern_room text,
+    building_present_situation_pattern_hall text,
+    building_present_situation_pattern_health text,
+    building_present_situation_pattern_compartmented text,
+    whether_there_is_manages_the_organization text,
+    total_price_Yuan text,
+    the_unit_price_a_Yuan_square_meter text,
+    the_berth_category text,
+    berth_shifting_total_area_square_meter text,
+    the_berth_total_price_yuan text,
+    the_note text,
+    serial_number text,
+    PRIMARY KEY (serial_number)
+);
+
+
+
